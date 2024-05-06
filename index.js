3d57f8324bf7b1de42ed7a4af6b05773e365a0a4
Modesto Amador <mofesdo.dev@gmail.com>
2024-05-05 17:35:21 -0700

    vowels done

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ function vowels(){
     console.log("there are x vowels in this string")
     let string = document.querySelector("#sentence").value;
     console.log(string)
-    string = string.toLowercase;
-    console.log(string)
     let count = 0;
     for(let i = 0; i < string.length; i++){
-        if(string[i] == "a" || string[i] == "e" ||string[i] == "i" ||string[i] == "o" ||string[i] == "u"){
+        if(string[i] == "a" || string[i] == "e" ||string[i] == "i" ||string[i] == "o" ||string[i] == "u" || string[i] == "A" || string[i] == "E" ||string[i] == "I" ||string[i] == "O" ||string[i] == "U"){
             count++
         }
     }
-    console.log(count)
+    alert(count)
 }
