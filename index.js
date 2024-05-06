8d6c8fb4a1a2c0c51fac21c9ef6bb5469fd3898e
Modesto Amador <mofesdo.dev@gmail.com>
2024-05-05 17:33:42 -0700

    working on vowel function

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 console.log("connected")
+document.querySelector("#submitBtn").addEventListener('click', vowels);
 
 function vowels(){
     console.log("there are x vowels in this string")
+    let string = document.querySelector("#sentence").value;
+    console.log(string)
+    string = string.toLowercase;
+    console.log(string)
+    let count = 0;
+    for(let i = 0; i < string.length; i++){
+        if(string[i] == "a" || string[i] == "e" ||string[i] == "i" ||string[i] == "o" ||string[i] == "u"){
+            count++
+        }
+    }
+    console.log(count)
 }
